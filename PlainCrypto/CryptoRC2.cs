265ca6901849fa463672f803ab21b3f7a35aed3f
gilberthdez <g3hdez@gmail.com>
2014-07-21 01:03:55 -0400

    Adding support for RC2 with CryptoRC2 class and test

---
+++ PlainCrypto/CryptoRC2.cs
@@ -0,0 +1,62 @@
+﻿using PlainCrypto.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PlainCrypto
+{
+    public sealed class CryptoRC2 : CryptoSymmetric 
+    {
+        /// <summary>
+        ///     Initialize a new instance of the PlainCrypto.CryptoRC2 class.
+        /// </summary>
+        /// 
+        /// <param name="key">
+        ///     Expects a valid key for the RC2 specification. 
+        ///     The valid key size are 40(5) to 128(16)-Bits(Bytes) in increments of 8(1)-Bits(Byte).
+        /// </param>
+        /// 
+        /// <exception name="Invalid key size">
+        ///     System.ArgumentException
+        ///         The supplied key is not valid for the RC2 specification.
+        /// </exception>
+        public CryptoRC2(byte[] key)
+        {
+            this.cryptoServiceProvider = new RC2CryptoServiceProvider();
+
+            if (!this.cryptoServiceProvider.ValidKeySize(key.Length * 8))
+            {
+                throw new System.ArgumentException("Invalid key size.", "key",
+                    new System.Exception("The supplied key is not valid for the RC2 specification. The valid key size are 40(5) to 128(16)-Bits(Bytes) in increments of 8(1)-Bits(Byte)."));
+            }
+
+            this.cryptoServiceProvider.Key = key;
+        }
+
+        /// <summary>
+        ///     Sets the initialization vector(IV) for the current PlainCrypto.CryptoRC2 instance
+        /// </summary>
+        /// 
+        /// <param name="iv">
+        ///     Expects a valid IV for the RC2 specification.
+        ///     The IV size must be 64(8)-Bits(Bytes)
+        /// </param>
+        /// 
+        /// <exception name="Invalid IV size.">
+        ///     System.ArgumentException
+        ///         The supplied IV is not valid for the RC2 specification.
+        /// </exception>
+        public override void SetIV(byte[] iv)
+        {
+            if (!(this.cryptoServiceProvider.LegalBlockSizes[0].MaxSize == iv.Length * 8))
+            {
+                throw new System.ArgumentException("Invalid IV size.",
+                    new System.Exception("The supplied IV is not valid for the RC2 specification. The IV size must be 64(8)-Bits(Bytes)"));
+            }
+
+            this.cryptoServiceProvider.IV = iv;
+        }
+    }
+}
---
+++ PlainCryptoTest/CryptoRC2_Test.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlainCrypto;
+using PlainCrypto.Abstract;
+
+namespace PlainCryptoTest
+{
+    [TestClass]
+    public class CryptoRC2_Test
+    {
+        [TestMethod]
+        public void CryptoRC2_40Bit_FixedIV_Test()
+        {
+            //Arrange
+            byte[] key = { 45, 6, 251, 89, 16 };
+            byte[] iv = { 63, 208, 159, 46, 37, 77, 1, 59 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            crypto.SetIV(iv);
+            string originalMessage = "I love cryptography and RC2 with a 40-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+
+        [TestMethod]
+        public void CryptoRC2_40Bit_RandomIV_Test()
+        {
+            //Arrange
+            byte[] key = { 45, 6, 251, 89, 16 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            string originalMessage = "I love cryptography and RC2 with a 40-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+
+        [TestMethod]
+        public void CryptoRC2_64Bit_FixedIV_Test()
+        {
+            //Arrange
+            byte[] key = { 158, 23, 64, 96, 57, 225, 36, 85 };
+            byte[] iv = { 63, 208, 159, 46, 37, 77, 1, 59 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            crypto.SetIV(iv);
+            string originalMessage = "I love cryptography and RC2 with a 64-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+
+        [TestMethod]
+        public void CryptoRC2_64Bit_RandomIV_Test()
+        {
+            //Arrange
+            byte[] key = { 158, 23, 64, 96, 57, 225, 36, 85 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            string originalMessage = "I love cryptography and RC2 with a 64-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+
+        [TestMethod]
+        public void CryptoRC2_128Bit_FixedIV_Test()
+        {
+            //Arrange
+            byte[] key = { 158, 23, 64, 96, 57, 225, 36, 85, 250, 23, 45, 94, 12, 36, 67, 170 };
+            byte[] iv = { 63, 208, 159, 46, 37, 77, 1, 59 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            crypto.SetIV(iv);
+            string originalMessage = "I love cryptography and RC2 with a 128-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+
+        [TestMethod]
+        public void CryptoRC2_128Bit_RandomIV_Test()
+        {
+            //Arrange
+            byte[] key = { 158, 23, 64, 96, 57, 225, 36, 85, 250, 23, 45, 94, 12, 36, 67, 170 };
+
+            CryptoSymmetric crypto = new CryptoRC2(key);
+            string originalMessage = "I love cryptography and RC2 with a 128-bits key";
+            string encryptedMessage = crypto.Encrypt(originalMessage);
+
+            //Act
+            string decryptedMessage = crypto.Decrypt(encryptedMessage);
+
+            //Assert
+            Assert.AreNotEqual(originalMessage, encryptedMessage);
+            Assert.AreEqual(originalMessage, decryptedMessage);
+        }
+    }
+}
